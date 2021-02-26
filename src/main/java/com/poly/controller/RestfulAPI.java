130935faac7af7680df99c5fe263ff237cb2fdf4
ToanLC4 <ToanLC4@fpt.com.vn>
2021-02-26 17:38:31 +0700

    a

--- src/main/java/com/poly/controller/RestfulAPI.java
+++ src/main/java/com/poly/controller/RestfulAPI.java
@@ -15,7 +15,7 @@ import com.poly.model.*;
 import com.poly.repositories.*;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:8080", "webdemodoan.herokuapp.com"})
+@CrossOrigin(origins = {"http://localhost:8080", "https://webdemodoan.herokuapp.com"})
 @RequestMapping("/api")
 public class RestfulAPI {
 	@Autowired
