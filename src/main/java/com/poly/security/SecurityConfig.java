ea4ed7d1e659273ad87972779c562c5c15d435c9
ToanLC4 <ToanLC4@fpt.com.vn>
2021-03-12 14:47:06 +0700

    a

--- src/main/java/com/poly/security/SecurityConfig.java
+++ src/main/java/com/poly/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -67,6 +68,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.cors().and().csrf().disable()
+			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
 			.authorizeRequests()
 //			.antMatchers("/css/**", "/js/**", "/images/**", "/webfonts/**").permitAll()
 			.antMatchers("/api/**").hasAuthority("ADMIN")
